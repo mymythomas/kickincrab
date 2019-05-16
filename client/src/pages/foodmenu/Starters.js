7969da97df3f2bdd9c04d8851f788bb4641d57d9
Mymy <thoaimydinh@gmail.com>
2019-05-15 18:33:07 -0700

    gdg

--- client/src/pages/foodmenu/Starters.js
+++ client/src/pages/foodmenu/Starters.js
@@ -2,27 +2,6 @@ import React from 'react';
 import foods from '../../food.json';
 import FoodCard from '../../components/FoodCard';
 
-<<<<<<< HEAD
-class Starters extends React.Component {
-    render() {
-        return (
-            <div>
-                <h1>Starters</h1>
-
-                {foods[0].items.map((item, index) => (
-                    <div key={index}>
-                        <FoodCard
-                            name={item.name}
-                            price={item.price}
-                            description={item.description}
-                        />
-                    </div>
-                ))}
-            </div>
-
-        );
-    }
-=======
 function Starters () {
     return (
         <div>
@@ -39,7 +18,6 @@ function Starters () {
             ))}
         </div>
     );
->>>>>>> 72c70ca6b0008b07f269d689b5d2e298b3b46b06
 }
 
 export default Starters;
