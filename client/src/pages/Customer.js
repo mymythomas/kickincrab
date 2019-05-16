7e31e78cd71d898fd066671ca77cb57a22dc210d
Thomas Dau <ThomasQDau@gmail.com>
2019-05-16 16:27:01 -0700

    now prevents time slots already taken up being rendered

--- client/src/pages/Customer.js
+++ client/src/pages/Customer.js
@@ -8,35 +8,69 @@ class Customer extends React.Component {
         name: "",
         phoneNum: "",
         reservationTime: "",
+        // Sets a default array of all available times to make reservation. Later will have another array in the state that compares itself to this array below and render according to what is missing.
+        currentReservation: [],
         availableTime: ["12:00 PM", "12:30 PM", "1:00 PM", "1:30 PM", "2:00 PM", "2:30 PM", "3:00 PM", "3:30 PM", "4:00 PM", "4:30 PM", "5:00 PM", "5:30 PM", "6:00 PM", "6:30 PM", "7:00 PM", "7:30 PM", "8:00 PM", "8:30 PM", "9:00 PM"]
+    }
+
+    componentDidMount() {
+        const stateChanger = this;
+        let availableTimeArr = this.state.availableTime;
+        axios.get('/api/allreservations').then(function (results) {
+            let data = results.data;
+            let currentReservation = [];
+            for (let i = 0; i < data.length; i++) {
+                currentReservation.push(data[i].reservationTime);
+            }
+            const filteredArray = availableTimeArr.filter(function (x) {
+                return currentReservation.indexOf(x) < 0;
+            });
+            stateChanger.setState({
+                availableTime: filteredArray
+            })
+        })
     }
 
     handleSubmit = event => {
         event.preventDefault();
+        // Create a post method to pass this.state to our server which will process it and add the information to the database AND use req.body.email to send an email containing information about the reservation. Also checks to see if the party is 8 or greater and adds a snipbit at the bottom explaining SC and split checks.
         axios.post('/api/reservation', this.state).then(function (req, res) {
         })
+
+        let availableTimeArr = this.state.availableTime;
+        let currentReservation = this.state.reservationTime
+        console.log(currentReservation)
+        console.log(availableTimeArr)
+        for (var i = availableTimeArr.length - 1; i >= 0; i--) {
+            if (availableTimeArr[i] === currentReservation) {
+                availableTimeArr.splice(i, 1);
+            }
+        }
+
         this.setState({
             name: "",
-            phoneNum:"",
+            phoneNum: "",
             reservationTime: "",
             numInParty: "",
-            email: ""
+            email: "",
+            availableTime: availableTimeArr
         });
+        console.log(this.state.availableTime);
     }
 
     handleReservation = data => event => {
         event.preventDefault();
-        console.log(data);
-        this.setState({reservationTime: data});
-
+        this.setState({ reservationTime: data });
     }
 
+    // Handles all input change. Depending on the input it will validate accordingly. Key is the parameter passed when the function is called and is linked to the key of the state.
     handleInputChange = key => event => {
         if (key === "phoneNum" && event.target.value <= 1) {
             return this.setState({
                 [key]: event.target.value
             });
         };
+        // The RegEx checks if ANY non number is inputed and stops the rest of the function from executing thus preventing the user from inputing anything but a number for phone number
         if (key === "phoneNum" && !/^\d+$/.test(event.target.value)) return;
         this.setState({
             [key]: event.target.value
@@ -45,8 +79,15 @@ class Customer extends React.Component {
 
     render() {
 
+        // Maps over array of available reservation time and return an array of buttons that will set state to the desired reservation time. Currently active reservation time will render green when clicked on
         const availableTime = this.state.availableTime.map(data => {
-            return (<button key={data} style={this.state.reservationTime === data ? {backgroundColor: 'green'}:{backgroundColor: 'pink'}} data-time={data} onClick={this.handleReservation(data)}>{data}</button>)
+            return (<button
+                key={data}
+                style={this.state.reservationTime === data ? { backgroundColor: 'green' } : { backgroundColor: 'pink' }}
+                data-time={data}
+                onClick={this.handleReservation(data)}
+            >
+                {data}</button>)
         })
 
         return (
@@ -54,33 +95,67 @@ class Customer extends React.Component {
                 <div>
                     <h1>Customer</h1>
                     <p>
-                       Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque velit, lobortis ut magna
-                    varius, blandit rhoncus sem. Morbi lacinia nisi ac dui fermentum, sed luctus urna tincidunt.
-                    Etiam ut feugiat ex. Cras non risus mi. Curabitur mattis rutrum ipsum, ut aliquet urna
-                    imperdiet ac. Sed nec nulla aliquam, bibendum odio eget, vestibulum tortor. Cras rutrum
-                    ligula in tincidunt commodo. Morbi sit amet mollis orci, in tristique ex. Donec nec ornare
-                    elit. Donec blandit est sed risus feugiat porttitor. Vestibulum molestie hendrerit massa non
-                    consequat. Vestibulum vitae lorem tortor. In elementum ultricies tempus. Interdum et
-                    malesuada fames ac ante ipsum primis in faucibus.
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque velit, lobortis ut magna
+                     varius, blandit rhoncus sem. Morbi lacinia nisi ac dui fermentum, sed luctus urna tincidunt.
+                     Etiam ut feugiat ex. Cras non risus mi. Curabitur mattis rutrum ipsum, ut aliquet urna
+                     imperdiet ac. Sed nec nulla aliquam, bibendum odio eget, vestibulum tortor. Cras rutrum
+                     ligula in tincidunt commodo. Morbi sit amet mollis orci, in tristique ex. Donec nec ornare
+                     elit. Donec blandit est sed risus feugiat porttitor. Vestibulum molestie hendrerit massa non
+                     consequat. Vestibulum vitae lorem tortor. In elementum ultricies tempus. Interdum et
+                     malesuada fames ac ante ipsum primis in faucibus.
             </p>
                 </div>
+
+                {/* Form to make reservation */}
                 <div id="reservation">
                     <form id="reservationForm">
                         <label htmlFor="name">Name to hold Reservation: </label>
-                        <input value={this.state.name} onChange={this.handleInputChange('name')} className="name" type="name" name="name" placeholder="Name"></input><br></br>
+                        <input
+                            value={this.state.name}
+                            onChange={this.handleInputChange('name')}
+                            className="name"
+                            type="name"
+                            name="name"
+                            placeholder="Name"
+                        >
+                        </input><br></br>
 
                         <div id="avail-time-btn">
                             <p>Available Reservation Times: </p>
                             {availableTime}
                         </div>
-                        <label htmlFor="numInParty"># of people in your party. (Please include children and babies as they ALSO FUCKINGG TAKE SPACE)</label>
-                        <input type="text" maxLength="2" style={{width: '27px'}} value={this.state.numInParty} onChange={this.handleInputChange('numInParty')}></input> <br></br>
+
+                        <label htmlFor="numInParty"># of people in your party. (Please include children and babies as they ALSO FUCKING TAKE SPACE)</label>
+                        <input
+                            type="text"
+                            maxLength="2"
+                            style={{ width: '25px' }}
+                            value={this.state.numInParty}
+                            onChange={this.handleInputChange('numInParty')}
+                        >
+                        </input> <br></br>
 
                         <label htmlFor="phone">Phone #: </label>
-                        <input onChange={this.handleInputChange('phoneNum')} className="phone" value={this.state.phoneNum} type="tel" maxLength="10" name="phone"></input><br></br>
+                        <input
+                            onChange={this.handleInputChange('phoneNum')}
+                            className="phone"
+                            value={this.state.phoneNum}
+                            type="tel"
+                            maxLength="10"
+                            name="phone"
+                        >
+                        </input><br></br>
 
                         <label htmlFor="email">Email: </label>
-                        <input onChange={this.handleInputChange('email')} style={{width: '200px'}} className="email" value={this.state.email} type="email" name="email"></input><br></br>
+                        <input
+                            onChange={this.handleInputChange('email')}
+                            style={{ width: '200px' }}
+                            className="email"
+                            value={this.state.email}
+                            type="email"
+                            name="email"
+                        >
+                        </input><br></br>
 
                         <button onClick={this.handleSubmit} className="submit-btn">Submit</button>
                     </form>
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require("express");
 var router = express.Router();
 var db = require("../models");
 
+// Sets up the email to be used to send reservation email Please no steal my PW Mymy :(
 var nodemailer = require('nodemailer');
 var transporter = nodemailer.createTransport({
     service: 'Gmail',
@@ -10,6 +11,12 @@ var transporter = nodemailer.createTransport({
         pass: 'tdftl123'
     }
 });
+
+router.get("/api/allreservations", function (req, res) {
+    db.Reservation.find({}).then(function (data) {
+        res.json(data);
+    })
+})
 
 router.post("/api/reservation", function (req, res) {
     db.Reservation.create({
@@ -21,11 +28,11 @@ router.post("/api/reservation", function (req, res) {
         res.end();
     });
 
-    let message = '<p> Hello, ' + req.body.name + '! We have received your reservation at ' + req.body.reservationTime + '. We will send you another email once your table is ready. Please be within the area and arrive promptly when you receive your second email. To be seated your entire party must be present. The host WILL perform a headcount.</p>'
-
+    let message = '<p> Hello, ' + req.body.name + '!</p> <p> We have received your reservation at ' + req.body.reservationTime + '. We will send you another email once your table is ready. Please be within the area and arrive promptly when you receive your second email. To be seated your entire party must be present. The host WILL perform a headcount.</p>'
     if (parseFloat(req.body.numInParty) >= 8) {
         message += "<p>Notice: Because your party contains 8 or more people. Our policy states that an automatic service charge of 18% will be added to your tab. Please note as well that your tab may only be split up to three ways. Any further questions can be directed to your server.</p>"
     }
+
     transporter.sendMail({
         from: 'kickincrabtesting@gmail.com',
         to: req.body.email,
