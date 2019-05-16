daded7622c0fad92c5e5a74785aa62f5968f0825
ThomasQDau <ThomasQDau@gmail.com>
2019-05-16 00:56:39 -0700

    sleeping

--- client/src/pages/Customer.js
+++ client/src/pages/Customer.js
@@ -6,7 +6,7 @@ class Customer extends React.Component {
         name: "",
         phoneNum: "",
         reservationTime: "",
-        availableTime: ["12:00", "12:30", "1:00", "1:30", "2:00", "2:30", "3:00", "3:30", "4:00", "4:30", "5:00", "5:30", "6:00", "6:30", "7:00", "7:30", "8:00", "8:30", "9:00"]
+        availableTime: ["12:00 PM", "12:30 PM", "1:00 PM", "1:30 PM", "2:00 PM", "2:30 PM", "3:00 PM", "3:30 PM", "4:00 PM", "4:30 PM", "5:00 PM", "5:30 PM", "6:00 PM", "6:30 PM", "7:00 PM", "7:30 PM", "8:00 PM", "8:30 PM", "9:00 PM"]
     }
 
     handleSubmit = event => {
