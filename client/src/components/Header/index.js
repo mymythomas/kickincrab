927901d1a1738707d42083913f9da410752f74ac
Thomas Dau <ThomasQDau@gmail.com>
2019-05-20 11:54:47 -0700

    added navigation and media query

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 
-function Header () {
+function Header() {
     return (
-       <h1 className="text-center">The Kickin' Crab</h1>
+        //    <h1 className="text-center">The Kickin' Crab</h1>
+        <div style={{padding: "25px 0 25px 0"}}>
+            <img style={{ margin: "auto", display: "block" }} alt="logo" src="logo.png"></img>
+        </div>
     )
 }
 
---
+++ client/src/components/Scroller/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Scroller() {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default scroller;
